aba9a09f7fda5eb703da1b258aca270ff7d83879
Adam Blake <adamblake@g.ucla.edu>
2021-07-29 12:30:01 -0700

    Update documentation, lock R, and speed up install
    - README now explains files
    - runtime now explicitly specifies R version at 4.1
    - Installs should be improved by using multiple cores

--- install.R
+++ install.R
@@ -1,4 +1,19 @@
-install.packages(c('remotes', 'learnr'))
+# use multiple cores if possible
+ncpus <- max(1L, parallel::detectCores())
+
+# CourseKata packages on CRAN
+install.packages(
+    c('supernova', 'mosaic', 'lsr', 'fivethirtyeight', 'Lock5withR', 'okcupiddata', 'dslabs'),
+    Ncpus = ncpus
+)
+
+# Other CRAN packages
+install.packages(
+    c('remotes', 'tidyverse', 'lme4'),
+    Ncpus = ncpus
+)
+
+# Non-CRAN packages
 remotes::install_github('datacamp/testwhat')
 remotes::install_github('UCLATALL/coursekata-r')
 coursekata::coursekata_install()
